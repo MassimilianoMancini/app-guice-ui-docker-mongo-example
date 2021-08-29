5860720efe6e21f146d4cc6ffab1a366119096df
MassimilianoMancini <massimiliano1.mancini@gmail.com>
2021-08-29 15:59:39 +0200

    fixed some exclusions and logger

--- school/src/main/java/com/example/school/app/swing/SchoolSwingApp.java
+++ school/src/main/java/com/example/school/app/swing/SchoolSwingApp.java
@@ -2,6 +2,8 @@ package com.example.school.app.swing;
 
 import java.awt.EventQueue;
 import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.example.school.guice.SchoolSwingMongoDefaultModule;
 import com.example.school.view.swing.StudentSwingView;
@@ -16,36 +18,29 @@ public class SchoolSwingApp implements Callable<Void> {
 
 	@Option(names = { "--mongo-host" }, description = "MongoDB host address")
 	private String mongoHost = "localhost";
-	
+
 	@Option(names = { "--mongo-port" }, description = "MongoDB host port")
 	private int mongoPort = 27017;
-	
+
 	@Option(names = { "--db-name" }, description = "Database name")
 	private String databaseName = "school";
-	
+
 	@Option(names = { "--db-collection" }, description = "Collection name")
 	private String collectionName = "student";
-	
-	
-	
+
 	public static void main(String[] args) {
 		new CommandLine(new SchoolSwingApp()).execute(args);
 	}
-	
+
 	@Override
 	public Void call() throws Exception {
 		EventQueue.invokeLater(() -> {
 			try {
-				Guice.createInjector (
-					new SchoolSwingMongoDefaultModule()
-						.mongoHost(mongoHost)
-						.mongoPort(mongoPort)
-						.databaseName(databaseName)
-						.collectionName(collectionName))
-					.getInstance(StudentSwingView.class)
-					.start();
+				Guice.createInjector(new SchoolSwingMongoDefaultModule().mongoHost(mongoHost).mongoPort(mongoPort)
+						.databaseName(databaseName).collectionName(collectionName)).getInstance(StudentSwingView.class)
+						.start();
 			} catch (Exception e) {
-				e.printStackTrace();
+				Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Exception", e);
 			}
 		});
 		return null;
